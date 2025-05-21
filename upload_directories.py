787c65effa54ee501b5f5f8e37efb51b613b3b17
Echo <97310557+DinoExho@users.noreply.github.com>
2025-05-21 13:24:47 +0100

    Update upload_directories.py

--- upload_directories.py
+++ upload_directories.py
@@ -20,14 +20,12 @@ def main():
                 "-F", f"content=@{full_github_path}",
                 f"https://www.pythonanywhere.com/api/v0/user/{username}/files/home/{username}/ccse2/{directory_path}/{file_name}"
             ]
-
+            print(f"https://www.pythonanywhere.com/api/v0/user/{username}/files/home/{username}/ccse2/{directory_path}/{file_name}")
             try:
                 print(f"uploading")
                 process = subprocess.run(curl_command, capture_output=True, text=True, check=True)
                 if process.returncode == 0:
                     print("SUCCESS:")
-                    if process.stdout:
-                        print(process.stdout.strip())
                 else:
                     print("FAIL")
                     if process.stdout:
